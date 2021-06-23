13c04f322b2e37b7423b0ceffec6232fc02baff5
InsYst3m <godlike.insyst3m@gmail.com>
2021-06-23 19:46:00 +0300

    Add project files.

---
+++ Web.API/Controllers/ClientCredentialsFlowController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles = "DaemonAppRole")]
+    public class ClientCredentialsFlowController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public string[] Get()
+        {
+            return Summaries;
+        }
+    }
+}
---
+++ Web.API/Controllers/RopcFlowController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace Web.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    [RequiredScope("Api.Access")]
+    public class RopcFlowController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "ROPC flow controller result.";
+        }
+    }
+}
